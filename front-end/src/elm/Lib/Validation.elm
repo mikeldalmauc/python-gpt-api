a1f5512b92523e3017988c4ceff7272baf8748ec
mikeldalmauc <mikeldalmauc@gmail.com>
2024-06-14 09:25:08 +0200

    ignored .env

---
+++ front-end/src/elm/Lib/Validation.elm
@@ -0,0 +1,53 @@
+module Lib.Validation exposing
+    ( Validation
+    , apply
+    , fail
+    , succeed
+    , withValidation
+    )
+
+
+type Validation a
+    = Success a
+    | Failure (List String)
+
+
+succeed : a -> Validation a
+succeed =
+    Success
+
+
+fail : String -> Validation a
+fail message =
+    Failure [ message ]
+
+
+apply : Validation a -> Validation (a -> b) -> Validation b
+apply va vf =
+    case ( vf, va ) of
+        ( Success f, Success a ) ->
+            Success (f a)
+
+        ( Failure es, Success _ ) ->
+            Failure es
+
+        ( Success _, Failure es ) ->
+            Failure es
+
+        ( Failure es1, Failure es2 ) ->
+            Failure (es1 ++ es2)
+
+
+withValidation :
+    { onSuccess : a -> b
+    , onFailure : List String -> b
+    }
+    -> Validation a
+    -> b
+withValidation { onSuccess, onFailure } va =
+    case va of
+        Success a ->
+            onSuccess a
+
+        Failure es ->
+            onFailure es
